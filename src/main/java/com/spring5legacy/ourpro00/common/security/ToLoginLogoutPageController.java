a3303636a96a2e4f8115f5a444acffae38e04f66
ronechild <ronechild@gmail.com>
2024-06-28 16:08:54 +0900

    security
    security

--- src/main/java/com/spring5legacy/ourpro00/common/security/ToLoginLogoutPageController.java
+++ src/main/java/com/spring5legacy/ourpro00/common/security/ToLoginLogoutPageController.java
@@ -10,7 +10,7 @@ public class ToLoginLogoutPageController {
 	@GetMapping("/login")
 	public String toLoginPage(String error, String logout, Model model) {
 		
-		return null;
+		return "common/login";
 	}
 	
 	@GetMapping("/logout")
